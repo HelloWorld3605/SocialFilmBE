81abc4cddb0795d7a6de9bf83873af1956808f9a
HelloWorld3605 <tuanhai362005t@gmail.com>
2026-04-05 00:19:38 +0700

    first commit ("tạo backend api và proxy")

---
+++ src/main/java/com/filmbe/FilmBeApplication.java
@@ -0,0 +1,13 @@
+package com.filmbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FilmBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FilmBeApplication.class, args);
+	}
+
+}
